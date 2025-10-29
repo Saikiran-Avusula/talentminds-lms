b39102a426460f2f2888154df3a685e2799d39d2
Saikiran-Avusula <saikiranavusula89@gmail.com>
2025-10-29 13:15:24 +0530

    installation of tailwind css

--- backend/src/main/java/com/talentminds/lms/TalentMindsLmsApplication.java
+++ backend/src/main/java/com/talentminds/lms/TalentMindsLmsApplication.java
@@ -1,13 +1,18 @@
 package com.talentminds.lms;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LmsApplication {
+public class TalentMindsLmsApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(TalentMindsLmsApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(LmsApplication.class, args);
+		SpringApplication.run(TalentMindsLmsApplication.class, args);
+		log.info("TalentMinds LMS App is running successfully....!");
 	}
 
 }
